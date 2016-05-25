250c48bbf052b5fd909814c5459190e26b31123d
John Moss <john@stolen-bikes.co.uk>
2016-05-25 11:45:29 +0100

    Update vatvalidation.py

--- python/vatvalidation.py
+++ python/vatvalidation.py
@@ -6,12 +6,12 @@ if __name__ == '__main__':
   
   #Check VAT Number is right number of characters for the format I'm using (not going to bother checking the actual format)
   if(len(vatnumber) != 11):
-    print "Please supply VAT Number in the format 123-4567-15"
+    print "Please supply VAT Number in the format GB123456715"
     exit()
     
   #split the VAT Number into the check digits and the rest
   checkdigit = int('-'+vatnumber[-2:])
-  rest = vatnumber[:9].replace('-','')
+  rest = vatnumber[2:9]
   
   ## Now start the formula
   multi = 8
